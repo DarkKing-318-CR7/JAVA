52a1e4baa0d18dfb9e1be5dc5097d80c19edfc9f
Chi <huuchinguyen241@gmail.com>
2025-03-16 12:27:13 +0700

    chi first commit

---
+++ src/main/java/com/example/pickleball_booking/controller/BookingController.java
@@ -0,0 +1,26 @@
+package com.example.pickleball_booking.controller;
+
+import com.example.pickleball_booking.model.Booking;
+import com.example.pickleball_booking.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.createBooking(booking);
+    }
+}
